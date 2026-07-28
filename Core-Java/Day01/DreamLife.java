e821c90f38f6100f72b30c18d9c3a1dae2dda3c0
Saisri Gandla <gandlasaisri895@gmail.com>
2026-07-28 10:17:29 +0530

    Day 1: Completed basic Java programs

---
+++ Core-Java/Day01/DreamLife.java
@@ -0,0 +1,24 @@
+public class DreamLife {
+    public static void main(String[] args) {
+System.out.println("========================================");
+System.out.println("            MY DREAM");
+System.out.println("========================================");
+System.out.println("Name            : Saisri Gandla");
+System.out.println();
+System.out.println("Dream Job       : Java Backend Developer");
+System.out.println();
+System.out.println("Target Company  :");
+System.out.println("- TCS");
+System.out.println("- Accenture");
+System.out.println("- Infosys");
+System.out.println("- Cognizant");
+System.out.println();
+System.out.println("Expected Salary : ₹50,000/month");
+System.out.println();
+System.out.println("Deadline        : 16 October 2026");
+System.out.println();
+System.out.println("Motto           :\n\"Consistency beats talent.\"");
+System.out.println();
+System.out.println("========================================");
+}
+}
---
+++ Core-Java/Day01/PersonalProfile.java
@@ -0,0 +1,17 @@
+public class PersonalProfile {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("          MY PERSONAL PROFILE");
+        System.out.println("=========================================");
+        System.out.println("Name            : Saisri Gandla");
+        System.out.println("Age             : 21");
+        System.out.println("Degree          : B.Tech (CSE)");
+        System.out.println("CGPA            : 8.42");
+        System.out.println("Goal            : Java Backend Developer");
+        System.out.println("Expected Salary : ₹50,000/month");
+        System.out.println("Country         : India");
+        System.out.println("Position        : Fresher");
+        System.out.println("=========================================");
+
+    }
+}
