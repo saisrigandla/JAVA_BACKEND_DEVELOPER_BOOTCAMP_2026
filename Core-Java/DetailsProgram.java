8562879c82f10ea2748dc8dbc921b4256058138d
Saisri Gandla <gandlasaisri895@gmail.com>
2026-07-28 18:04:00 +0530

    Unwanted files

--- Core-Java/DetailsProgram.java
+++
@@ -1,8 +0,0 @@
-public class DetailsProgram {
-    public static void main(String[] args) {
-        String name="Saisri";
-        String address="India";
-        String college="SVSGOI";
-        System.out.print(name+"\n"+address+'\n'+college);
-    }
-}
