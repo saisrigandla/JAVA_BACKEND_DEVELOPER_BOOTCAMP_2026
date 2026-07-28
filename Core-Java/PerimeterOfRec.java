e33c74c8581823f07bac2699fad483c565ff1eec
Saisri Gandla <gandlasaisri895@gmail.com>
2026-07-28 18:05:10 +0530

    Unwanted files

--- Core-Java/PerimeterOfRec.java
+++
@@ -1,7 +0,0 @@
-public class PerimeterOfRec {
-    public static void main(String[] args) {
-        int l=20;
-        int w=10;
-        System.out.println("Perimeter of Rectangle: "+ 2*(l+w));
-    }
-}
