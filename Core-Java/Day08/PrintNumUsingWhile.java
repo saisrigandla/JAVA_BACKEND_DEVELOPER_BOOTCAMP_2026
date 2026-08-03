b0969ddf7e47e99473fbff408b7410857e7ecc4b
Saisri Gandla <gandlasaisri895@gmail.com>
2026-08-03 21:20:06 +0530

    Day08 : While Loop

---
+++ Core-Java/Day08/PrintNumUsingWhile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PrintNumUsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+        int i=1;
+        while(i<=num){
+            System.out.println(i);
+            i++;
+        }
+        sc.close();
+
+    }
+}
