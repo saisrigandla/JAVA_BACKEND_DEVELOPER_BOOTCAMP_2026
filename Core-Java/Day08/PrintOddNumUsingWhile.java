0b565b3e3b789ed75fed4946597dd004ea003edc
Saisri Gandla <gandlasaisri895@gmail.com>
2026-08-05 23:02:34 +0530

    Day 08 & 09 : Continuation While Loop problems.

---
+++ Core-Java/Day08/PrintOddNumUsingWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PrintOddNumUsingWhile {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int i = 1;
+        System.out.print("Enter a number: ");
+        int n = sc .nextInt();
+        while(i<=n){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+            i++;
+        }
+        sc.close();
+    }
+}
