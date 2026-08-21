33e021dc082606e855df0883dd43a7bb0e52c5ac
Saisri-535 <gandlasaisri895@gmail.com>
2026-08-21 21:23:48 +0530

    Palindrome.java

---
+++ Core-Java/Day10/Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rev =0;
+        int n = sc.nextInt();
+        int org=n;
+        while(n>0){
+            int rem = n%10;
+            rev = rev*10+rem;
+            n=n/10;
+        }
+        if(org==rev){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
+    }
+}
