fd735c114c45a3861fa4a0d79cd7879a9d03d685
Saisri-535 <gandlasaisri895@gmail.com>
2026-08-23 09:01:35 +0530

    PrintName.java

---
+++ Core-Java/Day10/PrintName.java
@@ -0,0 +1,11 @@
+public class PrintName {
+    public static void main(String[] args) {
+        String name1="Sairam";
+        String name2="Saisri";
+        for(int i=1; i<=10; i++){
+            System.out.println(i+" "+name1);
+        }
+       System.out.println(name2);
+       
+    }
+}
